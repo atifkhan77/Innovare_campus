52a2407179b095582869847faeb2cf9838cd3a58
Muhammad Atif <atifm1190@gmail.com>
2024-11-26 18:49:00 +0500

    adding some dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
