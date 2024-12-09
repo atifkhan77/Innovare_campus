e5ee06f532ff91917f78bcfccadf2ae9673acfa4
Muhammad Atif <atifm1190@gmail.com>
2024-12-09 20:38:34 +0500

    lost and found ui updated

--- lib/Views/web/admin_dashboard/LostandFoundManage/LostAndFoundScreen.dart
+++ lib/Views/web/admin_dashboard/LostandFoundManage/LostAndFoundScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:intl/intl.dart'; // Import intl for date formatting
+import 'package:intl/intl.dart';
 import 'package:innovare_campus/Views/web/admin_dashboard/LostandFoundManage/EditLostFoundScreen.dart';
 import 'package:innovare_campus/model/lostfind.dart';
 
@@ -40,70 +40,98 @@ class LostAndFoundScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Manage Lost and Found'),
+        title: const Text('Manage Lost and Found',
+            style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromRGBO(49, 42, 119, 1),
+        iconTheme: IconThemeData(color: Colors.white),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('lost_found').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                "assets/Splash.png"), // Add your background image here
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('lost_found').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
 
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(child: Text('No items found.'));
-          }
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return const Center(child: Text('No items found.'));
+            }
 
-          final items = snapshot.data!.docs
-              .map((doc) => LostFoundItem.fromDocument(doc))
-              .toList();
+            final items = snapshot.data!.docs
+                .map((doc) => LostFoundItem.fromDocument(doc))
+                .toList();
 
-          return ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              return Card(
-                margin: const EdgeInsets.all(8),
-                child: ListTile(
-                  leading: GestureDetector(
-                    onTap: () => launchURL(item.imageUrl), // Launch image URL in browser
-                    child: CachedNetworkImage(
-                      imageUrl: item.imageUrl,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => const CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => const Icon(Icons.image),
+            return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  color: const Color.fromRGBO(
+                      255, 255, 255, 0.8), // Semi-transparent white
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15), // Rounded corners
+                  ),
+                  shadowColor: Colors.black.withOpacity(0.3),
+                  elevation: 8,
+                  child: ListTile(
+                    leading: GestureDetector(
+                      onTap: () => launchURL(
+                          item.imageUrl), // Launch image URL in browser
+                      child: CachedNetworkImage(
+                        imageUrl: item.imageUrl,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.image),
+                      ),
+                    ),
+                    title: Text(
+                      item.description,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Location: ${item.location}',
+                            style: const TextStyle(fontSize: 14)),
+                        Text('Date: ${formatDate(item.timestamp)}',
+                            style: const TextStyle(fontSize: 14)),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () => editItem(context, item),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                          onPressed: () async {
+                            await deleteItem(item.id);
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  title: Text(item.description),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Location: ${item.location}'),
-                      Text('Date: ${formatDate(item.timestamp)}'), // Display formatted date and time
-                    ],
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.edit, color: Colors.blue),
-                        onPressed: () => editItem(context, item),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () async {
-                          await deleteItem(item.id);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
